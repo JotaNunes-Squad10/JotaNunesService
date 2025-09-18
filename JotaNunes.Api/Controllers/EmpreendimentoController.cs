744f26e57bac5dad219226a4b3eb91ff3c243604
joaodionizio <joao.dionizio@souunit.com.br>
2025-09-18 20:30:57 -0300

    CRUD completo

--- JotaNunes.Api/Controllers/EmpreendimentoController.cs
+++ JotaNunes.Api/Controllers/EmpreendimentoController.cs
@@ -1,12 +1,14 @@
 ﻿using JotaNunes.Api.Controllers.Base;
 using JotaNunes.Application.UseCases.Empreendimento.Commands.Requests;
+using JotaNunes.Application.UseCases.Empreendimento.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JotaNunes.Api.Controllers;
 
 public class EmpreendimentoController(
-    IMediator mediator
+    IMediator mediator,
+    IEmpreendimentoQueries queries
 ) : BaseController(mediator)
 {
     [HttpPost("CreateEmpreendimento")]
@@ -14,4 +16,28 @@ public class EmpreendimentoController(
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> CreateEmpreendimentoAsync([FromBody] CreateEmpreendimentoRequest request)
         => CustomResponse(await Send(request));
+
+    [HttpPut("UpdateEmpreendimento")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> UpdateEmpreendimentoAsync([FromBody] UpdateEmpreendimentoRequest request)
+        => CustomResponse(await Send(request));
+
+    [HttpGet("GetAllEmpreendimentos")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> GetAllEmpreendimentosAsync()
+        => CustomResponse(await queries.GetAllAsync());
+
+    [HttpGet("GetEmpreendimentoById/{id:long}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> GetEmpreendimentoByIdAsync(long id)
+        => CustomResponse(await queries.GetByIdAsync(id));
+
+    [HttpDelete("DeleteEmpreendimento/{id:long}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> DeleteEmpreendimentoAsync(long id)
+        => CustomResponse(await Send(new DeleteEmpreendimentoRequest(id)));
 }
--- JotaNunes.Api/Controllers/MarcaController.cs
+++ JotaNunes.Api/Controllers/MarcaController.cs
@@ -1,12 +1,14 @@
 ﻿using JotaNunes.Api.Controllers.Base;
 using JotaNunes.Application.UseCases.Marca.Commands.Requests;
+using JotaNunes.Application.UseCases.Marca.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JotaNunes.Api.Controllers;
 
 public class MarcaController(
-    IMediator mediator
+    IMediator mediator,
+    IMarcaQueries queries
 ) : BaseController(mediator)
 {
     [HttpPost("CreateMarca")]
@@ -14,4 +16,28 @@ public class MarcaController(
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> CreateMarcaAsync([FromBody] CreateMarcaRequest request)
         => CustomResponse(await Send(request));
+
+    [HttpPut("UpdateMarca")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> UpdateMarcaAsync([FromBody] UpdateMarcaRequest request)
+        => CustomResponse(await Send(request));
+
+    [HttpGet("GetAllMarcas")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> GetAllMarcasAsync()
+        => CustomResponse(await queries.GetAllAsync());
+
+    [HttpGet("GetMarcaById/{id:long}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> GetMarcaByIdAsync(long id)
+        => CustomResponse(await queries.GetByIdAsync(id));
+
+    [HttpDelete("DeleteMarca/{id:long}")]
+    [ProducesResponseType(StatusCodes.Status20OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> DeleteMarcaAsync(long id)
+        => CustomResponse(await Send(new DeleteMarcaRequest(id)));
 }
