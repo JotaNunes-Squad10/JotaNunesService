7b19e90b230da37bb1aaf510d5f7dab8de4e25aa
Richard Souza <souzza.richard25@gmail.com>
2025-09-26 20:34:35 +0000

    feat: Implements user deletion

--- JotaNunes.Infrastructure.CrossCutting.Integration/Requests/Keycloak/UserRequest.cs
+++ JotaNunes.Infrastructure.CrossCutting.Integration/Requests/Keycloak/UserRequest.cs
@@ -2,10 +2,10 @@ namespace JotaNunes.Infrastructure.CrossCutting.Integration.Requests.Keycloak;
 
 public class UserRequest
 {
-    public required string Username { get; set; }
+    public string? Username { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public string? Email { get; set; }
-    public required bool Enabled { get; set; }
+    public bool? Enabled { get; set; }
     public Dictionary<string, List<string>>? Attributes { get; set; }
 }
