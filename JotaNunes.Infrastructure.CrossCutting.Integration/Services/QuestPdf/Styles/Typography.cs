a880ed862ccc85832037be35ed7d7f36e579d989
joaodionizio <joao.dionizio@souunit.com.br>
2025-10-23 21:08:28 -0300

    fix
    # Conflicts:
    #	JotaNunes.Infrastructure.CrossCutting.Integration/Services/QuestPdf/Documents/DocumentoEmpreendimento.cs

--- JotaNunes.Infrastructure.CrossCutting.Integration/Services/QuestPdf/Styles/Typography.cs
+++ JotaNunes.Infrastructure.CrossCutting.Integration/Services/QuestPdf/Styles/Typography.cs
@@ -27,4 +27,11 @@ public static class Typography
         .FontColor("#000000")
         .FontSize(12)
         .LineHeight(2);
+    
+    public static TextStyle Small => TextStyle
+        .Default
+        .FontFamily("Calibri")
+        .FontColor("#555555") 
+        .FontSize(10)
+        .LineHeight(1);
 }
